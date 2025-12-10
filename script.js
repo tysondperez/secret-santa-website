f6997cd9f1d0c136a7d3b0c63b55b0af193a91a7
tysondperez <122401749+tysondperez@users.noreply.github.com>
2025-12-09 22:24:28 -0800

    ux changes

--- script.js
+++ script.js
@@ -9,7 +9,13 @@ function handleSubmit() {
     input.focus();
 
     const ind = answers.indexOf(text);
-    document.getElementById("output").textContent = ind >= 0 ? digits[ind] : "Try Again.";
+    if (ind >= 0){
+        const pos = indToPos(ind);
+        document.getElementById("output").textContent = "Correct!\nThe " + pos + " digit is: " + digits[ind];
+    }
+    else {
+        document.getElementById("output").textContent = "Try Again.";
+    }
 }
 
 document.getElementById("userInput").addEventListener("keypress", function(event) {
@@ -17,3 +23,15 @@ document.getElementById("userInput").addEventListener("keypress", function(event
     handleSubmit();
   }
 });
+
+function indToPos(ind){
+    switch (ind){
+        case 0: 
+            return "first";
+        case 1:
+            return "second";
+        case 2:
+            return "third";
+    }
+    return "";
+}
