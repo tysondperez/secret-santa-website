e3cd08c297923988fea5131ceafbc6539f0151d0
tysondperez <122401749+tysondperez@users.noreply.github.com>
2025-12-09 20:17:47 -0800

    prototype with functional input/output logic

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+let answers = ["Answer1", "Answer2", "Answer3"];
+let digits = ["Digit1", "Digit2", "Digit3"];
+
 function handleSubmit() {
-    const text = document.getElementById("userInput").value;
-    document.getElementById("output").textContent = text;
+    const input = document.getElementById("userInput")
+    const text = input.value;
+    if (text.trim() === "") return;
+    input.value = "";
+    input.focus();
+
+    const ind = answers.indexOf(text);
+    document.getElementById("output").textContent = ind >= 0 ? digits[ind] : "Try Again.";
 }
+
+document.getElementById("userInput").addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    handleSubmit();
+  }
+});
